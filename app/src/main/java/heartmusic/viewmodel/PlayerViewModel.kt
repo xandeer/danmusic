244cf0c18b8aa467ca00b9dffcb4883b75bfb84d
Kevin Du <kkxandeer@gmail.com>
2023-01-22 16:13:22 +0800

    Feat: Add a progress slider

--- app/src/main/java/heartmusic/viewmodel/PlayerViewModel.kt
+++ app/src/main/java/heartmusic/viewmodel/PlayerViewModel.kt
@@ -1,10 +1,11 @@
 package heartmusic.viewmodel
 
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import androidx.media3.common.MediaItem
+import heartmusic.data.PlaylistQuerySong
 
 class PlayerViewModel : ViewModel() {
   var isPlaying by mutableStateOf(false)
@@ -13,8 +14,31 @@ class PlayerViewModel : ViewModel() {
   var currentIndex by mutableStateOf(-1)
     private set
 
-  fun play(index: Int) {
+  var playlistId by mutableStateOf(-1L)
+    private set
+
+  var songs = emptyList<PlaylistQuerySong>()
+    private set
+
+  val playingSong by derivedStateOf { songs.getOrNull(currentIndex) }
+
+  fun play(playlist: Long, songs: List<PlaylistQuerySong>, song: PlaylistQuerySong) {
+    this.songs = songs
+    playlistId = playlist
     isPlaying = true
+    currentIndex = songs.indexOf(song)
+  }
+
+  fun appendSongs(songs: List<PlaylistQuerySong>) {
+    this.songs = this.songs + songs
+  }
+
+  fun next() {
+    require(currentIndex < songs.size - 1) { "No next song" }
+    currentIndex = currentIndex.inc()
+  }
+
+  fun updateByPosition(index: Int) {
     currentIndex = index
   }
 
@@ -24,5 +48,15 @@ class PlayerViewModel : ViewModel() {
 
   fun toggle() {
     isPlaying = !isPlaying
+  }
+
+  var positionMs by mutableStateOf(0L)
+    private set
+  var durationMs by mutableStateOf(0L)
+    private set
+
+  fun updatePosition(position: Long, duration: Long) {
+    positionMs = position
+    durationMs = duration
   }
 }
