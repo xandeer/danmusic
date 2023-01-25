0f2a37d528b13de18d8885d7b3afeca6caa7f7d2
Kevin Du <kkxandeer@gmail.com>
2023-01-25 15:55:40 +0800

    Feat: Loading screen

---
+++ app/src/main/kotlin/heartmusic/ui/Util.kt
@@ -0,0 +1,21 @@
+package heartmusic.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+fun Modifier.swallowClick(): Modifier = clickableWithoutRipple {}
+
+private fun Modifier.clickableWithoutRipple(
+  enabled: Boolean = true,
+  onClick: () -> Unit,
+): Modifier = composed {
+  clickable(
+    indication = null,
+    interactionSource = remember { MutableInteractionSource() },
+    enabled = enabled,
+    onClick = onClick,
+  )
+}
