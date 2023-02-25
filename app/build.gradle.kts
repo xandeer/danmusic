fa0bdbb2eb5c24bfe944de9f765f1e6e2896ca97
Kevin Du <kkxandeer@gmail.com>
2023-02-25 15:20:32 +0800

    Deps: Update kotlin and compose compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,4 +78,13 @@ dependencies {
   }
 
   androidTestImplementation(libs.bundles.test.android)
+
+  constraints {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.10") {
+      because("kotlin-stdlib-jdk7 is now a part of kotlin-stdlib")
+    }
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10") {
+      because("kotlin-stdlib-jdk8 is now a part of kotlin-stdlib")
+    }
+  }
 }
